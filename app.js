4f399a4aa6a7bf5b11fb169d198314df8b8b6dbf
Konstantin Vasilyev <hello.vasilyev@gmail.com>
2024-01-25 10:33:37 +0300

    Joi and Celebrate Fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { errors } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const auth = require('./middlewares/auth');
@@ -17,8 +17,22 @@ mongoose.connect(MONGO_URL);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
